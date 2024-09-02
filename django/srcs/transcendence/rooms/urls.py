435f2440ed94dc0b4c65451c986ea8be33b266c4
agserran <agustinsscc01@gmail.com>
2024-09-02 20:23:55 +0200

    done

--- django/srcs/transcendence/rooms/urls.py
+++ django/srcs/transcendence/rooms/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from .views import (
     rooms_view,
+    rooms_create,
+    rooms_join,
+    rooms_join_public,
+    rooms_join_private,
 )
 
 urlpatterns = [
     path('rooms/', rooms_view, name='rooms'),
+    path('rooms/create', rooms_create, name='rooms_create'),
+    path('rooms/join', rooms_join, name='rooms_join'),
+    path('rooms/join/public', rooms_join_public, name='rooms_join_public'),
+    path('rooms/join/private', rooms_join_private, name='rooms_join_private'),
 ]
--- django/srcs/transcendence/rooms/views.py
+++ django/srcs/transcendence/rooms/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
 
 def rooms_view(request):
-    context = {
-        'game_mode' : '1vs1',
-        'team1' : 'teamPepe',
-        'team2' : 'teamAgus',
-        'room_code' : 'Abaunfjkdf'
-    }
-
     if 'HX-Request' in request.headers:
-        return render(request, 'rooms/rooms.html', context)
+        return render(request, 'rooms/rooms.html')
     else:
-        return render(request, 'rooms/rooms_full.html', context)
+        return render(request, 'rooms/rooms_full.html')
+
+def rooms_create(request):
+    if 'HX-Request' in request.headers:
+        return render(request, 'rooms/rooms_create.html')
+    else:
+        return render(request, 'rooms/rooms_create_full.html')
+    
+def rooms_join(request):
+    if 'HX-Request' in request.headers:
+        return render(request, 'rooms/rooms_join.html')
+    else:
+        return render(request, 'rooms/rooms_join_full.html')
+    
+def rooms_join_public(request):
+    if 'HX-Request' in request.headers:
+        return render(request, 'rooms/rooms_join_public.html')
+    else:
+        return render(request, 'rooms/rooms_join_public_full.html')
+    
+def rooms_join_private(request):
+    if 'HX-Request' in request.headers:
+        return render(request, 'rooms/rooms_join_private.html')
+    else:
+        return render(request, 'rooms/rooms_join_private_full.html')
