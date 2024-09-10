1475871110daddf2626d8794ed3b98cddebed95a
Samuel Cordero <samuelcorderorodrigo@gmail.com>
2024-09-10 12:46:46 +0200

    added socket_ctr to user model; added logic for increasing/decreasing ctr on connect/disconnect

--- django/srcs/transcendence/social/consumers.py
+++ django/srcs/transcendence/social/consumers.py
@@ -1,13 +1,30 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
+from .models import User as OurUser
 
 class OnlineConsumer(AsyncWebsocketConsumer):
 
-	async def connect(self):
-		await self.accept()
-		await self.send("connected")
+    async def connect(self):
+        self.user = self.scope["user"]
 
-	async def disconnect(self, close_code):
-		pass
+        if self.user is not None:
+            try:
+                user = await OurUser.objects.aget(name=self.user.username)
+                user.socket_ctr += 1
+                await user.asave()
+                await self.accept()
+            except OurUser.DoesNotExist:
+                pass
 
-	async def receive(self, text_data):
-		await self.send(text_data)
+    async def disconnect(self, close_code):
+        self.user = self.scope["user"]
+
+        if self.user is not None:
+            try:
+                user = await OurUser.objects.aget(name=self.user.username)
+                user.socket_ctr -= 1
+                await user.asave()
+            except OurUser.DoesNotExist:
+                pass
+
+    async def receive(self, text_data):
+        pass
