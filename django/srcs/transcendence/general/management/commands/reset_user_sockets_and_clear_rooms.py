b4a6fb68df0419e468ea5538a02f5180a171304d
Samuel Cordero <samuelcorderorodrigo@gmail.com>
2024-09-11 16:58:35 +0200

    reconstructed general app; added reset task for socket_ctr and rooms

---
+++ django/srcs/transcendence/general/management/commands/reset_user_sockets_and_clear_rooms.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from social.models import User as OurUser
+from rooms.models import Room
+
+class Command(BaseCommand):
+    help = 'Restore socket_ctr to one on all users and remove all rooms'
+
+    def handle(self, *args, **kwargs):
+        print("\033[93mExecuting reset_user_sockets_and_clear_rooms!\033[0m")
+        print("Setting all socket_ctr to 0...")
+        OurUser.objects.all().update(socket_ctr=0)
+        print("Deleting rooms...")
+        Room.objects.all().delete()
+        print("\033[92mTask complete!\033[0m")
--- django/utils/entrypoint.sh
+++ django/utils/entrypoint.sh
@@ -1,3 +1,4 @@
 python3 manage.py makemigrations
 python3 manage.py migrate
+python3 manage.py reset_user_sockets_and_clear_rooms
 python3 manage.py runserver 0:443
