fad564750f775ddb9bdb78d96ba893277a6ce456
Samuel Cordero <samuelcorderorodrigo@gmail.com>
2024-09-15 12:36:56 +0200

    superuser automatization for django from env

---
+++ django/srcs/transcendence/general/management/commands/createsuperuser_from_env.py
@@ -0,0 +1,33 @@
+import os
+from django.contrib.auth.management.commands import createsuperuser
+from django.core.management import CommandError
+from django.contrib.auth import get_user_model
+
+class Command(createsuperuser.Command):
+    help = 'Create a superuser with password provided from environment variables.'
+
+    def add_arguments(self, parser):
+        super(Command, self).add_arguments(parser)
+        parser.add_argument(
+            '--password', dest='password', default=None,
+            help='Specifies the password for the superuser.',
+        )
+
+    def handle(self, *args, **options):
+        username = options.get('username')
+        password = options.get('password') or os.getenv('DJANGO_ROOT_PASSWORD', '')
+        database = options.get('database')
+
+        UserModel = get_user_model()
+
+        if UserModel._default_manager.db_manager(database).filter(username=username).exists():
+            print(f"User '{username}' already exists. Superuser creation skipped.")
+            return
+
+        super(Command, self).handle(*args, **options)
+
+        if password:
+            user = self.UserModel._default_manager.db_manager(database).get(username=username)
+            user.set_password(password)
+            user.save()
+            print(f"Password set for user: {username}")
--- django/utils/entrypoint.sh
+++ django/utils/entrypoint.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py reset_user_sockets_and_clear_rooms
+if [ -n "$DJANGO_ROOT_PASSWORD" ] && [ -n "$DJANGO_ROOT_USER" ]; then
+    python3 manage.py createsuperuser_from_env --username "$DJANGO_ROOT_USER" --noinput --email root@here.com
+fi
 python3 manage.py runserver 0:443
