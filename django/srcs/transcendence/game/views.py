ca436fe497e72fc7e8e9675f82f1f6444ee0ff18
Pepe Segura <psegura-@student.42madrid.com>
2024-09-17 17:05:35 +0200

    same error than game not found

--- django/srcs/transcendence/game/views.py
+++ django/srcs/transcendence/game/views.py
@@ -6,7 +6,7 @@ from game.models import Game
 def game_view(request, game_id):
     game_instance = get_object_or_404(Game, game_id=game_id)
     if game_instance.end_time:
-        raise Http404("You can't join an already finished game")
+        raise Http404("No Game matches the given query.")
     if 'HX-Request' in request.headers:
         return render(request, 'game.html')
     else:
