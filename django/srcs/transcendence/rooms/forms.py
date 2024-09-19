089b74f305b5dd9770a33f3b2790a2a89ac07489
Agustin Serrano Cucarella <agserran@c3r10s4.42madrid.com>
2024-09-19 17:52:22 +0200

    falta el formulario de create

--- django/srcs/transcendence/rooms/forms.py
+++ django/srcs/transcendence/rooms/forms.py
@@ -2,40 +2,36 @@ from django import forms
 from .models import Room
 
 class RoomForm(forms.ModelForm):
-    game_mode = forms.ChoiceField(
-        choices=Room.GAME_MODES,
-        widget=forms.Select(attrs={
-            'style': 'color: #28df28; background-color: #181A1B; border: 1px solid #181A1B; padding: 10px; border-radius: 4px; font-family: "Bebas Neue", sans-serif; border-color: #181A1B;',
-        }),
-        label='Game Mode'
-    )
-    
-    is_public = forms.ChoiceField(
-        choices=[('public', 'Public'), ('private', 'Private')],
-        widget=forms.Select(attrs={
-            'style': 'color: #28df28; background-color: #181A1B; border: 1px solid #181A1B; padding: 10px; border-radius: 4px; font-family: "Bebas Neue", sans-serif; border-color: #181A1B;',
-        }),
-        label='Privacy'
-    )
-    
     class Meta:
         model = Room
         fields = ['game_mode', 'is_public']
+
+       
+        game_mode = forms.ChoiceField(
+            choices=Room.GAME_MODES,
+            widget=forms.Select(attrs={
+                'class': 'form-select'}),
+            label='Game Mode'
+        )
+        
+        is_public = forms.ChoiceField(
+            choices=[('public', 'Public'), ('private', 'Private')],
+            widget=forms.Select(attrs={'class': 'form-select'}),
+            label='Privacy'
+        )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
 class JoinPrivateForm(forms.Form):
     room_id = forms.CharField(
         max_length=6,
         label='Room id',
-        widget=forms.TextInput(attrs={
-            'style': 'color: #28df28; background-color: #181A1B; border-color: #181A1B;  font-family: "Bebas Neue", sans-serif',
-            'class': 'form-control custom-input',
-        })
     )
 
     class Meta:
         model = Room
         fields = ['room_id']
-
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
