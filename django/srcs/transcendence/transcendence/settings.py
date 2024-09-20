864430822af2500f5049a4c3e298ba3f6af0391b
Samuel Cordero <samuelcorderorodrigo@gmail.com>
2024-09-20 16:55:04 +0200

    secret key from .env

--- django/srcs/transcendence/transcendence/settings.py
+++ django/srcs/transcendence/transcendence/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/ 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-otm3=!xw(s09l*+vjsg_acvro!un#f26-uprba_56j1&zxu!uc'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
